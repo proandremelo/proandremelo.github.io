6f074ea32f42ddcd522a6ab4185c44784a91e909
André Melo <proandremelo@gmail.com>
2022-11-14 17:15:48 -0300

    App.css - Content.css - Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,19 +8,50 @@ import trybetunes from '../images/Tunes.png'
 import starwars from '../images/starwars.png'
 
 function Content() {
+
   return(
     <main>
       <h1 className="introduction">Hi, I'm André!</h1>
       <h2>Frontend Developer, Musician and Nature lover</h2>
-      <fieldset className="projects">
-        <legend align="center">Projects</legend>
-        <img src={trivia} alt="trivia" />
-        <img src={starwars} alt="starwars" />
-        <img src={trybetunes} alt="trybetunes" />
-      </fieldset>
+      <h2 className="proj-text">And these are some projects that I'm working on</h2>
+      <div className="projects">
+        <a
+          target="_blank"
+          rel="no noreferrer"
+          href="https://trivia-andremelo.vercel.app/"
+        >
+          <img src={trivia} alt="trivia" />
+        </a>
+        <a
+          target="_blank"
+          rel="no noreferrer"
+          href="https://star-wars-andremelo.vercel.app/"
+        >
+          <img src={starwars} alt="starwars" />
+        </a>
+        <a
+          target="_blank"
+          rel="no noreferrer"
+          href="https://trybetunes-andremelo.vercel.app/"
+        >
+          <img src={trybetunes} alt="trybetunes" />
+        </a>
+      </div>
       <div className="icons">
-        <BsGithub className="github-icon" />
-        <AiFillLinkedin className="linkedin-icon" />
+        <a
+          target="_blank"
+          rel="no noreferrer"
+          href="https://github.com/proandremelo"
+        >
+          <BsGithub className="github-icon" />  
+        </a>
+        <a
+          target="_blank"
+          rel="no noreferrer"
+          href="https://www.linkedin.com/in/andremelodev/"
+        >
+          <AiFillLinkedin className="linkedin-icon" />
+        </a>
       </div>
     </main>
   );
